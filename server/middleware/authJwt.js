539e3c99e51dc22cf9900d8d7d5808be4d3cf265
Berlin_Joseph <l.berlinjoe@hotmail.com>
2023-12-24 01:07:20 +0530

    backend update

--- server/middleware/authJwt.js
+++ server/middleware/authJwt.js
@@ -18,6 +18,7 @@ const authJwt = () => {
       "/api/v1/users/auth/login",
       { url: /\/api\/v1\/products(.*)/, methods: ["GET", "OPTIONS"] },
       { url: /\/api\/v1\/category(.*)/, methods: ["GET", "OPTIONS"] },
+      { url: /\/public\/uploads(.*)/, methods: ["GET", "OPTIONS"] },
     ],
   });
 };
