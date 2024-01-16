073697aae4a84585d9fdcf35f58193ff3bc1e4e3
Berlin_Joe <berlin_joe@192.168.1.79>
2024-01-16 15:17:41 +0530

    redux

--- web-admin/src/pages/Products.jsx
+++ web-admin/src/pages/Products.jsx
@@ -17,148 +17,161 @@ const Products = () => {
   const products = useSelector(selectAllProducts);
 
   //
-  const handleAddProducts = (e) => {
-    e.preventDefault();
-    dispatch(
-      addProduct({ name, description, category, featured, brand, price })
-    );
-  };
-  return (
-    <div className=" container mx-auto h-screen p-10">
-      <div className="flex flex-col ">
-        <div className="flex flex-col">
-          <label htmlFor="" className="mt-5 mb-2">
-            Name
-          </label>
-          <input
-            type="text"
-            className="border border-dark-purple rounded-md p-2"
-            onChange={(e) => setName(e.target.value)}
-          />
-        </div>
-        <div className="flex flex-col">
-          <label htmlFor="" className="mt-5  mb-2">
-            Description
-          </label>
-          <input
-            type="text"
-            className="border border-dark-purple rounded-md p-2"
-            onChange={(e) => setDescription(e.target.value)}
-          />
-        </div>
-        <div className="grid grid-cols-2 justify-between gap-10">
-          <div className="flex flex-col">
-            <label htmlFor="" className="mt-5 mb-2 ">
-              Category
-            </label>
-            <select
-              id="category"
-              className="border border-dark-purple rounded-md p-2"
-              onChange={(e) => setCategory(e.target.value)}
-            >
-              <option value="option1">Option 1</option>
-              <option value="option2">Option 2</option>
-            </select>
-          </div>
-          <div className="flex flex-col">
-            <label htmlFor="" className="mt-5 mb-2">
-              Featured
-            </label>
-            <select
-              id="category"
-              className="border border-dark-purple rounded-md p-2"
-              onChange={(e) => setFeatured(e.target.value)}
-            >
-              <option value="option1">Option 1</option>
-              <option value="option2">Option 2</option>
-            </select>
-          </div>
-        </div>
-        <div className="grid grid-cols-2 justify-between gap-10">
-          <div className="flex flex-col">
-            <label htmlFor="" className="mt-5  mb-2">
-              Brand
-            </label>
-            <input
-              type="text"
-              className="border border-dark-purple rounded-md p-2"
-              onChange={(e) => setBrand(e.target.value)}
-            />
-          </div>
-          <div className="flex flex-col">
-            <label htmlFor="" className="mt-5 mb-2">
-              Price
-            </label>
-            <input
-              type="text"
-              className="border border-dark-purple rounded-md p-2"
-              onChange={(e) => setPrice(e.target.value)}
-            />
-          </div>
-        </div>
-        <button
-          className=" bg-dark-purple mt-5 py-5 rounded-md text-white"
-          onClick={handleAddProducts}
-        >
-          Add Products
-        </button>
-      </div>
-      <div className=" border border-dark-purple my-5" />
-      {/*  */}
-      <h1 className=" text-2xl">Product List</h1>
-      <div className="flex flex-col">
-        <div className="overflow-x-auto sm:-mx-6 lg:-mx-8">
-          <div className="inline-block min-w-full py-2 sm:px-6 lg:px-8">
-            <div className="overflow-hidden">
-              <table className="min-w-full text-left text-sm font-light">
-                <thead className="border-b font-medium dark:border-neutral-500">
-                  <tr>
-                    <th scope="col" className="px-6 py-4">
-                      No
-                    </th>
-                    <th scope="col" className="px-6 py-4">
-                      Product Name
-                    </th>
-                    <th scope="col" className="px-6 py-4">
-                      Product Description
-                    </th>
-                    <th scope="col" className="px-6 py-4">
-                      Action
-                    </th>
-                  </tr>
-                </thead>
-                {products.map((data) => (
-                  <tbody>
-                    <tr className="border-b dark:border-neutral-500">
-                      <td className="whitespace-nowrap px-6 py-4 font-medium">
-                        1
-                      </td>
-                      <td className="whitespace-nowrap px-6 py-4">
-                        {data.name}
-                      </td>
-                      <td className="whitespace-nowrap px-6 py-4">
-                        {data.description}
-                      </td>
-                      <td className="whitespace-nowrap px-6 py-4">
-                        <div className="flex gap-5">
-                          <button>
-                            <FaEdit className="text-2xl text-blue-900" />
-                          </button>
-                          <button>
-                            <FaTrash className="text-2xl text-red-900" />
-                          </button>
-                        </div>
-                      </td>
-                    </tr>
-                  </tbody>
-                ))}
-              </table>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
+ const handleAddProducts = (e) => {
+   e.preventDefault();
+   dispatch(
+     addProduct({ name, description, category, featured, brand, price })
+   );
+   setName("");
+   setDescription("");
+   setCategory(null);
+   setFeatured(null);
+   setBrand("");
+   setPrice("");
+ };
+
+ return (
+   <div className=" container mx-auto h-screen p-10">
+     <div className="flex flex-col ">
+       <div className="flex flex-col">
+         <label htmlFor="" className="mt-5 mb-2">
+           Name
+         </label>
+         <input
+           type="text"
+           className="border border-dark-purple rounded-md p-2"
+           value={name}
+           onChange={(e) => setName(e.target.value)}
+         />
+       </div>
+       <div className="flex flex-col">
+         <label htmlFor="" className="mt-5  mb-2">
+           Description
+         </label>
+         <input
+           type="text"
+           className="border border-dark-purple rounded-md p-2"
+           value={description}
+           onChange={(e) => setDescription(e.target.value)}
+         />
+       </div>
+       <div className="grid grid-cols-2 justify-between gap-10">
+         <div className="flex flex-col">
+           <label htmlFor="" className="mt-5 mb-2 ">
+             Category
+           </label>
+           <select
+             id="category"
+             className="border border-dark-purple rounded-md p-2"
+             value={category}
+             onChange={(e) => setCategory(e.target.value)}
+           >
+             <option className=" bg-dark-purple">Option 1</option>
+             <option className=" bg-dark-purple">Option 2</option>
+           </select>
+         </div>
+         <div className="flex flex-col">
+           <label htmlFor="" className="mt-5 mb-2">
+             Featured
+           </label>
+           <select
+             id="category"
+             className="border border-dark-purple rounded-md p-2"
+             value={featured}
+             onChange={(e) => setFeatured(e.target.value)}
+           >
+             <option value="option1">Option 1</option>
+             <option value="option2">Option 2</option>
+           </select>
+         </div>
+       </div>
+       <div className="grid grid-cols-2 justify-between gap-10">
+         <div className="flex flex-col">
+           <label htmlFor="" className="mt-5  mb-2">
+             Brand
+           </label>
+           <input
+             type="text"
+             className="border border-dark-purple rounded-md p-2"
+             value={brand}
+             onChange={(e) => setBrand(e.target.value)}
+           />
+         </div>
+         <div className="flex flex-col">
+           <label htmlFor="" className="mt-5 mb-2">
+             Price
+           </label>
+           <input
+             type="text"
+             className="border border-dark-purple rounded-md p-2"
+             value={price}
+             onChange={(e) => setPrice(e.target.value)}
+           />
+         </div>
+       </div>
+       <button
+         className=" bg-dark-purple mt-5 py-5 rounded-md text-white"
+         onClick={handleAddProducts}
+       >
+         Add Products
+       </button>
+     </div>
+     <div className=" border border-dark-purple my-5" />
+     {/*  */}
+     <h1 className=" text-2xl">Product List</h1>
+     <div className="flex flex-col">
+       <div className="overflow-x-auto sm:-mx-6 lg:-mx-8">
+         <div className="inline-block min-w-full py-2 sm:px-6 lg:px-8">
+           <div className="overflow-hidden">
+             <table className="min-w-full text-left text-sm font-light">
+               <thead className="border-b font-medium dark:border-neutral-500">
+                 <tr>
+                   <th scope="col" className="px-6 py-4">
+                     No
+                   </th>
+                   <th scope="col" className="px-6 py-4">
+                     Product Name
+                   </th>
+                   <th scope="col" className="px-6 py-4">
+                     Product Description
+                   </th>
+                   <th scope="col" className="px-6 py-4">
+                     Action
+                   </th>
+                 </tr>
+               </thead>
+               {products.map((data) => (
+                 <tbody>
+                   <tr className="border-b dark:border-neutral-500">
+                     <td className="whitespace-nowrap px-6 py-4 font-medium">
+                       1
+                     </td>
+                     <td className="whitespace-nowrap px-6 py-4">
+                       {data.name}
+                     </td>
+                     <td className="whitespace-nowrap px-6 py-4">
+                       {data.description}
+                     </td>
+                     <td className="whitespace-nowrap px-6 py-4">
+                       <div className="flex gap-5">
+                         <button>
+                           <FaEdit className="text-2xl text-blue-900" />
+                         </button>
+                         <button>
+                           <FaTrash className="text-2xl text-red-900" />
+                         </button>
+                       </div>
+                     </td>
+                   </tr>
+                 </tbody>
+               ))}
+             </table>
+           </div>
+         </div>
+       </div>
+     </div>
+   </div>
+ );
 };
 
 export default Products;
--- web-admin/src/redux/slice/ProductSlice.js
+++ web-admin/src/redux/slice/ProductSlice.js
@@ -3,12 +3,14 @@ import { createSlice } from "@reduxjs/toolkit";
 const initialState = {
   products: [
     {
+      id: 1,
       name: "Product_2",
       description: "Product 2 description",
       richDescription: "Product 2 richDescription",
       brand: "Product 2 brand",
     },
     {
+      id: 2,
       name: "Product_1",
       description: "Product 1 description",
       richDescription: "Product 1 richDescription",
@@ -27,12 +29,23 @@ const productSlice = createSlice({
     addProduct: (state, action) => {
       state.products.push(action.payload);
     },
-    updateProduct: (state, action) => {},
-    deleteProduct: (state, action) => {},
+    updateProduct: (state, action) => {
+      state.products = state.products.map((products) =>
+        products.id === action.payload ? action.payload : task
+      );
+    },
+    setSelectedProducts: (state, action) => {
+      state.selectedProducts = action.payload;
+    },
+    deleteProduct: (state, action) => {
+      state.products = state.products.filter(
+        (product) => product.id !== action.payload.id
+      );
+    },
   },
 });
 
-export const { addProduct, updateProduct, deleteProduct } =
+export const { addProduct, updateProduct, setSelectedProducts, deleteProduct } =
   productSlice.actions;
 export const selectAllProducts = (state) => state.product.products;
 export default productSlice.reducer;
