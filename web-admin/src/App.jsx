46af30ea310067b92742b24e997d40b7696aec78
Berlin_Joe <berlin_joe@192.168.1.13>
2024-01-18 10:28:09 +0530

    bug fixed

--- web-admin/src/App.jsx
+++ web-admin/src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom"; 
 import SideBar from "./components/Sidebar/SideBar";
 import Dashboard from "./pages/Dashboard";
 import Products from "./pages/Products";
