23cfa67509c9c907cada44ec5f6f5a4e4ad9f1a6
Berlin_Joe <berlin_joe@192.168.1.13>
2024-01-18 10:08:38 +0530

    bug fixed

--- web-admin/src/App.jsx
+++ web-admin/src/App.jsx
@@ -13,8 +13,7 @@ const App = () => {
         <div className="flex">
           <SideBar />
           <Routes>
-            <Route index path="/dashboard" element={<Dashboard />} />
-            <Route index element={<Dashboard />} />
+            <Route path="/dashboard" element={<Dashboard />} />
             <Route path="/products" element={<Products />} />
             <Route path="/category" element={<Category />} />
           </Routes>
