a14bd8d9b9a37af90ea2a10b32743d4135697f91
Berlin_Joe <berlin_joe@Berlin-Joes-MacBook-Pro.local>
2024-02-24 10:07:23 +0530

    new updated ui

--- server/server.js
+++ server/server.js
@@ -22,7 +22,7 @@ app.options("*", cors());
 //middleware
 app.use(express.json());
 app.use(morgan("tiny"));
-app.use(authJwt());
+// app.use(authJwt());
 app.use(errorHandler);
 app.use("/public/uploads", express.static(__dirname + "/public/uploads"));
 
